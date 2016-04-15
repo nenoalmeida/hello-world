78aef2ed7e6103790c43001b0fbabc208a01cd1d
NenoAlmeida <nenoxvx@icloud.com>
2016-04-15 10:55:02 -0300

    Adding new definitions on Aplication Controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,10 @@ class ApplicationController < ActionController::Base
   def new
   end
   def create
-    render plain: params[:article].inspect
+    @article = Article.new(params[:article])
+
+    @article.save
+    redirect_to @article
+
   end
 end
